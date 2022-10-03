09c8cf7bcfc4f9f2ef2b943206013f94e44e4c45
vovuas2003 <89464038+vovuas2003@users.noreply.github.com>
2022-10-03 17:01:38 +0300

    Add files via upload

---
+++ 3.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define S 100
+
+int check(char *, char* , char* , int);
+
+int main() {
+    char inp[S], temp;
+    int i = 0;
+    while(1) {
+        if(scanf("%2hhx", &temp) != EOF) {
+            inp[i] = temp;
+            i++;
+        } else {
+            break;
+        }
+    }
+    int n = 256, ma = 0, res = 0;
+    char etaoin_up[12] = {'U', 'L', 'D', 'R', 'H', 'S', 'N', 'I', 'O', 'A', 'T', 'E'};
+    char etaoin_low[12] = {'u', 'l', 'd', 'r', 'h', 's', 'n', 'i', 'o', 'a', 't', 'e'};
+    char* brute = (char*)malloc(i * sizeof(char));
+    for(int x = 0; x < 256; x++) {
+        for(int j = 0; j < i; j++) {
+            brute[j] = inp[j] ^ x;
+        }
+        res = check(etaoin_up, etaoin_low, brute, i);
+        if(res > ma) {
+            ma = res;
+            n = x;
+        }
+    }
+    free(brute);
+    if(n == 256) {
+        printf("No printable strings!\n");
+        return 0;
+    }
+    printf("%d ", n);
+    for(int j = 0; j < i; j++) {
+        printf("%c", inp[j] ^ n);
+    }
+    printf("\n");
+    return 0;
+}
+
+int check(char* etaoin_up, char* etaoin_low, char* s, int l) {
+    int otv = 0;
+    char t;
+    for(int i = 0; i < l; i++) {
+        t = s[i];
+        if((32 <= t) && (t <= 126) && (t != 38)) {
+            for(int j = 0; j < 12; j++) {
+                if((t == etaoin_up[j]) || (t == etaoin_low[j])) {
+                    otv += 1 + j;
+                    break;
+                }
+            }
+        } else {
+            return -1;
+        }
+    }
+    return otv;
+}
