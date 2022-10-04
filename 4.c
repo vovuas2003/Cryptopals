8b8ab8673ef0a865fb2d05723e0c584e0eeda3a8
vovuas2003 <89464038+vovuas2003@users.noreply.github.com>
2022-10-04 11:57:36 +0300

    Update 4.c

--- 4.c
+++ 4.c
@@ -1,51 +1,58 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int check(char *, char* , char* , int);
-int one();
+#define S 100
+
+int check(char*, char*, char*, int);
+int one(char*, int, int);
 
 int main() {
+    char inp[S];
+    char buf[3];
+    buf[2] = '\0';
+    int l;
     for(int i = 0; i < 327; i++) {
-        printf("%d ", i + 1);
-        one();
+        scanf("%s", inp);
+        l = strlen(inp) / 2;
+        char* norm_inp = (char*)malloc(l * sizeof(char));
+        for(int j = 0; j < l; j++) {
+            buf[0] = inp[j * 2];
+            buf[1] = inp[j * 2 + 1];
+            sscanf(buf, "%2hhx", &norm_inp[j]);
+        }
+        one(norm_inp, l, i);
+        free(norm_inp);
     }
     return 0;
 }
 
-int one() {
-    char inp[30], temp;
-    int i = 30;
-    for(int on = 0; on < 30; on ++) {
-        scanf("%2hhx", &temp);
-        inp[on] = temp;
-    }
-    /*
-    int n = 256, ma = 0, res = 0;
+int one(char* inp, int l, int i) {
+    int ma = 0, res = 0;
+    char x = 0, n;
+    int f = 1;
     char etaoin_up[12] = {'U', 'L', 'D', 'R', 'H', 'S', 'N', 'I', 'O', 'A', 'T', 'E'};
     char etaoin_low[12] = {'u', 'l', 'd', 'r', 'h', 's', 'n', 'i', 'o', 'a', 't', 'e'};
-    char* brute = (char*)malloc(i * sizeof(char));
-    for(int x = 0; x < 256; x++) {
-        for(int j = 0; j < i; j++) {
+    char* brute = (char*)malloc(l * sizeof(char));
+    for(int i = 0; i < 256; i++) {
+        for(int j = 0; j < l; j++) {
             brute[j] = inp[j] ^ x;
         }
-        res = check(etaoin_up, etaoin_low, brute, i);
+        res = check(etaoin_up, etaoin_low, brute, l);
         if(res > ma) {
             ma = res;
             n = x;
+            f = 0;
         }
+        x++;
     }
     free(brute);
-    if(n == 256) {
-        printf("No printable strings!\n");
+    if(f) {
         return 0;
     }
-    printf("%d ", n);
-    for(int j = 0; j < i; j++) {
+    printf("%d %d ", i, n);
+    for(int j = 0; j < l; j++) {
         printf("%c", inp[j] ^ n);
-    }
-    */
-    for(int on = 0; on < 30; on ++) {
-        printf("%2x", inp[on]);
     }
     printf("\n");
     return 0;
@@ -56,10 +63,10 @@ int check(char* etaoin_up, char* etaoin_low, char* s, int l) {
     char t;
     for(int i = 0; i < l; i++) {
         t = s[i];
-        if(((t >= '0') && (t <= '9')) || ((t >= 'a') && (t <= 'z')) || ((t >= 'A') && (t <= 'Z')) || (t == ' ') || (t == '\n')) {
+        if(((t >= 32) && (t <= 126)) || (t == '\n')) {
             for(int j = 0; j < 12; j++) {
                 if((t == etaoin_up[j]) || (t == etaoin_low[j])) {
-                    otv += 1 + j;
+                    otv += 12 + 3 * (j + 1);
                     break;
                 }
             }
