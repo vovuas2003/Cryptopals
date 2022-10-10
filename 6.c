1ee50ac63abaa35fa188aebf4a8bf10e913fbf68
vovuas2003 <89464038+vovuas2003@users.noreply.github.com>
2022-10-10 11:53:02 +0300

    Update 6.c

--- 6.c
+++ 6.c
@@ -1,21 +1,31 @@
+/*
+cmd run command
+6.exe < 6.txt > out.txt 2> key.txt
+*/
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <float.h>
 
-#define ONE 100
-#define S 5000
-#define MA_KS 40
+#define ONE 100 /* max len of one string in input */
+#define S 5000 /* max input len */
+#define MA_KS 40 /* max possible len of key */
 
 void base64(char*, char*, char*, int, int, char*);
 char find(char, char*);
 int hamming(char*, char*, int);
+int bruteforce(char*, char*, int, int, char*, char*);
+int check(char*, char*, char*, int);
 
 int main() {
 
     char table[64] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
                       'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
                       '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '/'};
+
+    char etaoin_up[12] = {'U', 'L', 'D', 'R', 'H', 'S', 'N', 'I', 'O', 'A', 'T', 'E'};
+    char etaoin_low[12] = {'u', 'l', 'd', 'r', 'h', 's', 'n', 'i', 'o', 'a', 't', 'e'};
 
     char one[ONE], inp[S], buf[4], temp_write[3];
     int lo, l = 0, i, j;
@@ -61,7 +71,76 @@ int main() {
             ks = i;
         }
     }
-    printf("%d\n", ks);
+    char* brute = (char*)malloc(((l / ks) + 1) * sizeof(char));
+    char* key = (char*)malloc(ks * sizeof(char));
+    int l_brute = 0;
+    for(i = 0; i < ks; i++) {
+        l_brute = 0;
+        for(j = i; j < l; j += ks) {
+            brute[l_brute] = inp[j];
+            l_brute++;
+        }
+        if(bruteforce(brute, key, l_brute, i, etaoin_up, etaoin_low)) {
+            fprintf(stderr, "No printable strings in %d block!", i + 1);
+            return 1;
+        }
+    }
+    free(brute);
+    for(i = 0; i < ks; i++) {
+        fprintf(stderr, "%c", key[i]);
+    }
+    for(i = 0; i < l; i++) {
+        printf("%c", inp[i] ^ key[i % ks]);
+    }
+    free(key);
+    return 0;
+}
+
+int check(char* etaoin_up, char* etaoin_low, char* s, int l) {
+    int otv = 0;
+    char t;
+    for(int i = 0; i < l; i++) {
+        t = s[i];
+        if(((t >= 32) && (t <= 126)) || (t == '\n')) {
+            for(int j = 0; j < 12; j++) {
+                if((t == etaoin_up[j]) || (t == etaoin_low[j])) {
+                    otv += 12 + 3 * (j + 1);
+                    break;
+                }
+                if(((t >= 'a') && (t <= 'z')) || ((t >= 'A') && (t <= 'Z')) || (t == ' ')) {
+                    otv += 7;
+                } else {
+                    otv -= 30;
+                }
+            }
+        } else {
+            return -1;
+        }
+    }
+    return otv;
+}
+
+int bruteforce(char* brute, char* key, int l_brute, int i, char* etaoin_up, char* etaoin_low) {
+    char* temp = (char*)malloc(l_brute * sizeof(char));
+    int ma = 0, res = 0, f = 1;
+    char x = 0, n;
+    for(int i = 0; i < 256; i++) {
+        for(int j = 0; j < l_brute; j++) {
+            temp[j] = brute[j] ^ x;
+        }
+        res = check(etaoin_up, etaoin_low, temp, l_brute);
+        if(res > ma) {
+            ma = res;
+            n = x;
+            f = 0;
+        }
+        x++;
+    }
+    free(temp);
+    if(f) {
+        return 1;
+    }
+    key[i] = n;
     return 0;
 }
 
