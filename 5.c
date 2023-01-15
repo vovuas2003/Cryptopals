02a750a4fed79d67319efcfa69b5a7d6f7557884
vovuas2003 <89464038+vovuas2003@users.noreply.github.com>
2023-01-15 16:55:23 +0300

    Update 5.c

--- 5.c
+++ 5.c
@@ -1,7 +1,24 @@
+/*
+Here is the opening stanza of an important work of the English language:
+
+Burning 'em, if you ain't quick and nimble
+I go crazy when I hear a cymbal
+
+Encrypt it, under the key "ICE", using repeating-key XOR.
+
+In repeating-key XOR, you'll sequentially apply each byte of the key;
+the first byte of plaintext will be XOR'd against I, the next C, the next E,
+then I again for the 4th byte, and so on.
+
+It should come out to:
+0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272
+a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f
+*/
+
 #include <stdio.h>
 #include <string.h>
 
-#define S 33
+#define S 33 /* max len of key */
 
 int main() {
     char t;
