5a276a122aedfa5eb1fae9dbf567e3052abd3c55
vovuas2003 <89464038+vovuas2003@users.noreply.github.com>
2023-01-15 16:50:43 +0300

    Update 3.c

--- 3.c
+++ 3.c
@@ -1,3 +1,9 @@
+/*
+The hex encoded string:
+1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736
+... has been XOR'd against a single character. Find the key, decrypt the message.
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
